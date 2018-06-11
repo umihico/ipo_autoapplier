0adb05144df18462e4dcb532d7f3cc069c41167e
umihico <umihico_dummy@users.noreply.github.com>
2018-06-11 14:19:10 +0900

    first test

---
+++ nomura.py
@@ -0,0 +1,22 @@
+from umihico_commons.chrome_wrapper import Chrome
+from passwords import passwords
+from selenium.webdriver.common.keys import Keys
+
+
+def test():
+    url = "https://hometrade.nomura.co.jp/web/rmfCmnCauSysLgiInitAction.do"
+    c = Chrome(cookie_key=None, headless=False)
+    c.get(url)
+    c.xpath(
+        "//input[@id='branchNo']")[0].send_keys(passwords['nomura_ht']['branch_no'])
+    c.xpath(
+        "//input[@id='accountNo']")[0].send_keys(passwords['nomura_ht']['account_no'])
+    c.xpath(
+        "//input[@id='passwd1']")[0].send_keys(passwords['nomura_ht']['password'])
+    c.xpath(
+        "//input[@id='passwd1']")[0].send_keys(Keys.RETURN)
+    c.get("https://hometrade.nomura.co.jp/web/rmfTrdStkIpoLstAction.do")
+
+
+if __name__ == '__main__':
+    test()
---
+++ passwords.py
@@ -0,0 +1,10 @@
+
+
+from passpacker import passwords
+
+nomura_ht = {
+    'branch_no': passwords['nomura_ht_branch_no'],
+    'account_no': passwords['nomura_ht_account_no'],
+    'password': passwords['nomura_ht_password'], }
+
+passwords = {"nomura_ht": nomura_ht}
